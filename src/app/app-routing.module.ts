d7dcfc9f66800470940d5881b7b5c6e5d901f8ce
Luiz Henrique Correia <luizhenriquecorreia1997@gmail.com>
2019-06-05 23:16:50 -0300

    add intro page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'feed', loadChildren: './feed/feed.module#FeedPageModule' }
+  { path: '', loadChildren: './intro/intro.module#IntroPageModule' },
+  { path: 'feed', loadChildren: './feed/feed.module#FeedPageModule' },
+  { path: 'intro', loadChildren: './intro/intro.module#IntroPageModule' }
 ];
 @NgModule({
   imports: [
@@ -11,4 +12,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
---
+++ src/app/intro/intro.page.ts
@@ -0,0 +1,22 @@
+import { TabsPage } from './../tabs/tabs.page';
+import { Component, OnInit } from '@angular/core';
+import { NavController, NavParams } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.page.html',
+  styleUrls: ['./intro.page.scss'],
+})
+export class IntroPage implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  goToFeed() {
+    this.router.navigate(['feed']);
+  }
+
+}
